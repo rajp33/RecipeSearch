8347ce9f9feb6f3ac9710e107e574659fc17ee07
Kyle Koo <koodotk@login-students.ccs.neu.edu>
2019-09-16 11:51:35 -0400

    forgot the c file

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-// Insert your code here
+#include <stdio.h>
+
+double power(double base, double n) {
+	if (n > 0) {
+		return base * power(base, n - 1);
+	}
+	else {
+		return 1;
+	}
+}
+
+int main() {
+	double i;
+	for (i = 1; i <= 10; i++) {
+		printf("2 to the power of %f is %f. \n", i, power(2, i));
+	}
+	return 0;
+}
